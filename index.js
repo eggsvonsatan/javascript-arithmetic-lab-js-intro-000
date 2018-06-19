342bc4f5e8a8d30c6a04fa596860e321419d6677
eggsvonsatan <elbadry.d@gmail.com>
2018-06-19 03:12:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function add (){
-  return 600 + 44
+function add () {
+  return 1 + 80
+} // 81
+function subtract () {
+  60 - 40
+} // 20
+
+function multiply () {
+  2 * 3.4
+} // 6.8 (there's that floating-point arithmetic again...)
+function divide() {
+  5.0 / 2.5
 }
